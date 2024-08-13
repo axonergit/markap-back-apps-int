28dbf24fe6edd3e84edb8eb3fb47ee5a1ed650f5
Sebastian Z. <sebyex304@gmail.com>
2024-08-13 20:22:53 -0300

    Creado el proyecto con IntelliJ

---
+++ MarkapBE/src/main/java/org/grupo1/markapbe/MarkapBeApplication.java
@@ -0,0 +1,13 @@
+package org.grupo1.markapbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkapBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkapBeApplication.class, args);
+	}
+
+}
