3dd026e8102cefd9e6017eefe178258488393057
SorongoTongo <alejoperve@gmail.com>
2024-08-18 19:25:44 -0300

    Feat: Agregadas todas las capas correspondientes al manejo de productos, posibilidad de crear productos y consultarlos.

---
+++ MarkapBE/src/main/java/org/grupo1/markapbe/persistence/entity/CategoryEntity.java
@@ -0,0 +1,29 @@
+package org.grupo1.markapbe.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Categories")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombreCategoria")
+    private String nombreCategoria;
+
+    /*
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ProductEntity> productos;
+*/
+}
---
+++ MarkapBE/src/main/java/org/grupo1/markapbe/persistence/entity/ProductEntity.java
@@ -0,0 +1,42 @@
+package org.grupo1.markapbe.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Products")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "imagen")
+    private String imagen;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precio")
+    private Float precio;
+
+    @Column(name = "detalles")
+    private String detalles;
+
+    @Column(name = "stock")
+    private int stock;
+
+    /* PARA LABURAR DESPUES
+    @ManyToOne
+    @JoinColumn(name = "idCategoria", nullable = false)
+    private CategoryEntity categoria;
+*/
+}
