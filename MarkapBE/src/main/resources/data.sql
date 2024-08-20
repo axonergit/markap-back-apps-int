6d894adabf9aea2cf6bb4811b8f680b6fad6d289
Sebastian Z. <sebyex304@gmail.com>
2024-08-20 18:56:20 -0300

    Te movi todo del command line runner al data.sql alejo para que sea mas facil. Ojo con los nombres en camel case que para pasarlo a una tabla hay que pasarlo a snake case.

---
+++ MarkapBE/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Categories (nombre_categoria) VALUES ('Figuras de Acción');
+INSERT INTO Categories (nombre_categoria) VALUES ('Cómics y Mangas');
+INSERT INTO Categories (nombre_categoria) VALUES ('Monedas y Billetes');
+INSERT INTO Categories (nombre_categoria) VALUES ('Autógrafos');
+INSERT INTO Categories (nombre_categoria) VALUES ('Juguetes Vintage');
+INSERT INTO Categories (nombre_categoria) VALUES ('Cartas Coleccionables');
+INSERT INTO Categories (nombre_categoria) VALUES ('Arte');
+INSERT INTO Categories (nombre_categoria) VALUES ('Antiguedades');
+INSERT INTO Categories (nombre_categoria) VALUES ('Música y Vinilos');
+INSERT INTO Categories (nombre_categoria) VALUES ('Libros Raros');
+INSERT INTO Categories (nombre_categoria) VALUES ('Relojes y Joyería');
+INSERT INTO Categories (nombre_categoria) VALUES ('Fotografía');
