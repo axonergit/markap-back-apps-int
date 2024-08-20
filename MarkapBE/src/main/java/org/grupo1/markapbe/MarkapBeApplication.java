45cee7a49e5b57ec49e2c057946f3bbc95411f04
Sebastian Z. <sebyex304@gmail.com>
2024-08-20 18:56:20 -0300

    Te movi todo del command line runner al data.sql alejo para que sea mas facil. Ojo con los nombres en camel case que para pasarlo a una tabla hay que pasarlo a snake case.

--- MarkapBE/src/main/java/org/grupo1/markapbe/MarkapBeApplication.java
+++ MarkapBE/src/main/java/org/grupo1/markapbe/MarkapBeApplication.java
@@ -20,7 +20,7 @@ public class MarkapBeApplication {
 
 
     @Bean
-    CommandLineRunner init(UserRepository userRepository, CategoryRepository categoryRepository) {
+    CommandLineRunner init(UserRepository userRepository) {
         return args -> {
             PermissionEntity createPermission = PermissionEntity.builder()
                     .name("CREATE")
@@ -35,43 +35,6 @@ public class MarkapBeApplication {
                     .name("SUSCRIBE")
                     .build();
 
-            //Todo lo que tiene que ver con la creación de categorías
-            CategoryEntity category1 = CategoryEntity.builder()
-                    .nombreCategoria("Figuras de Acción")
-                    .build();
-            CategoryEntity category2 = CategoryEntity.builder()
-                    .nombreCategoria("Cómics y Mangas")
-                    .build();
-            CategoryEntity category3 = CategoryEntity.builder()
-                    .nombreCategoria("Monedas y Billetes")
-                    .build();
-            CategoryEntity category4 = CategoryEntity.builder()
-                    .nombreCategoria("Autógrafos")
-                    .build();
-            CategoryEntity category5 = CategoryEntity.builder()
-                    .nombreCategoria("Juguetes Vintage")
-                    .build();
-            CategoryEntity category6 = CategoryEntity.builder()
-                    .nombreCategoria("Cartas Coleccionables")
-                    .build();
-            CategoryEntity category7 = CategoryEntity.builder()
-                    .nombreCategoria("Arte")
-                    .build();
-            CategoryEntity category8 = CategoryEntity.builder()
-                    .nombreCategoria("Antiguedades")
-                    .build();
-            CategoryEntity category9 = CategoryEntity.builder()
-                    .nombreCategoria("Música y Vinilos")
-                    .build();
-            CategoryEntity category10 = CategoryEntity.builder()
-                    .nombreCategoria("Libros Raros")
-                    .build();
-            CategoryEntity category11 = CategoryEntity.builder()
-                    .nombreCategoria("Relojes y Joyería")
-                    .build();
-            CategoryEntity category12 = CategoryEntity.builder()
-                    .nombreCategoria("Fotografía")
-                    .build();
 
             RoleEntity roleUser = RoleEntity.builder().roleEnum(RoleEnum.USUARIO).permissionSet(Set.of(readPermission, suscribePermission)).build();
             RoleEntity roleAdmin = RoleEntity.builder().roleEnum(RoleEnum.ADMIN).permissionSet(Set.of(readPermission, suscribePermission, createPermission, deletePermission)).build();
@@ -82,7 +45,7 @@ public class MarkapBeApplication {
             userRepository.saveAll(List.of(userMaestro, userComun));
 
             //guardamos las cateorias en las tablas
-            categoryRepository.saveAll(List.of(category1, category2,category3,category4,category5,category6,category7,category8,category9,category10,category11,category12));
+
         };
     }
 
