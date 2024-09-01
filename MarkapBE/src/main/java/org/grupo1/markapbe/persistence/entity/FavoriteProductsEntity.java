e89f94b0633180befee2dbdecb349dc94f5e1ec6
SorongoTongo <alejoperve@gmail.com>
2024-09-01 15:35:37 -0300

    Feat: Agregada la funcionalidad de agregar productos a favoritos y de consultar los productos likeados por user

---
+++ MarkapBE/src/main/java/org/grupo1/markapbe/persistence/entity/FavoriteProductsEntity.java
@@ -0,0 +1,41 @@
+package org.grupo1.markapbe.persistence.entity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+
+//IMPORTANTE: VER COMO HACER PARA QUE NO SE REPITAN FILAS CON MISMO ID_USER Y MISMO ID_PRODUCT
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "FavoriteProducts")
+public class FavoriteProductsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_user", nullable = false)
+    private Long id_user;
+
+    @Column(name = "id_producto", nullable = false)
+    private Long id_product;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_user",insertable = false, updatable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private ProductEntity product;
+}
