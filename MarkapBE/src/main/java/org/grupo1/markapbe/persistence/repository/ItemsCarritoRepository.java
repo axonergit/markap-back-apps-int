afc078ff968348678b87d23974aea4279c24cc51
mathiuSpv <mathius998@gmail.com>
2024-09-04 15:14:44 -0300

    feat(carrito): add CarritoService and implement pending functionalities in repositories

--- MarkapBE/src/main/java/org/grupo1/markapbe/persistence/repository/ItemsCarritoRepository.java
+++ MarkapBE/src/main/java/org/grupo1/markapbe/persistence/repository/ItemsCarritoRepository.java
@@ -1,4 +1,22 @@
 package org.grupo1.markapbe.persistence.repository;
 
-public interface ItemsCarritoRepository {
+import org.grupo1.markapbe.persistence.entity.ItemsCarritoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+
+@Repository
+public interface ItemsCarritoRepository extends JpaRepository<ItemsCarritoEntity, Long> {
+    /**
+     * Finds a specific item in a cart based on cart ID and product ID.
+     *
+     * @param carritoId the ID of the cart
+     * @param productoId the ID of the product
+     * @return an Optional containing the found ItemsCarritoEntity, or empty if not found
+     */
+    Optional<ItemsCarritoEntity> findByCarritoIdAndProductId(@Param("carrito_id") Long carritoId, @Param("product_id") Long productId);
 }
--- MarkapBE/src/main/java/org/grupo1/markapbe/service/CarritoService.java
+++ MarkapBE/src/main/java/org/grupo1/markapbe/service/CarritoService.java
@@ -1,4 +1,66 @@
 package org.grupo1.markapbe.service;
 
+import org.grupo1.markapbe.persistence.entity.CarritoEntity;
+import org.grupo1.markapbe.persistence.entity.ItemsCarritoEntity;
+import org.grupo1.markapbe.persistence.repository.CarritoRepository;
+import org.grupo1.markapbe.persistence.repository.ItemsCarritoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class CarritoService {
+
+    @Autowired
+    private CarritoRepository carritoRepository;
+
+    @Autowired
+    private ItemsCarritoRepository itemsCarritoRepository;
+
+    public CarritoEntity getCarritoById(Long id) {
+        Optional<CarritoEntity> carritoEntity = carritoRepository.findById(id);
+        return carritoEntity.orElse(null);
+    }
+
+    /**
+     * I
+     *
+     */
+    public ItemsCarritoEntity addItemToCart(Long carritoId, Long productoId, int cantidad) {
+        // Verificar si el item ya existe en el carrito
+        Optional<ItemsCarritoEntity> existingItem = itemsCarritoRepository.findByCarritoIdAndProductId(carritoId, productoId);
+        if (existingItem.isPresent()) {
+            //Existe, solo se modifica Cantidad.
+            ItemsCarritoEntity item = existingItem.get();
+            item.setAmount(item.getAmount() + 1);
+            return itemsCarritoRepository.save(item);
+        } else {
+            //Si no existe lo crea
+            CarritoEntity carrito = this.getCarritoById(carritoId);
+            //Falta agregar PorductoEntity
+
+            ItemsCarritoEntity newItem = ItemsCarritoEntity.builder()
+                    .carrito(carrito)
+                    //Falta agregar Producto
+                    .amount(1)
+                    .build();
+
+            return itemsCarritoRepository.save(newItem);
+        }
+
+    }
+
+    public ItemsCarritoEntity removeItemToCart(Long carritoId, Long productoId) {
+        Optional<ItemsCarritoEntity> existingItemOpt = itemsCarritoRepository.findByCarritoIdAndProductId(carritoId, productoId);
+
+        if (existingItemOpt.isPresent()) {
+            ItemsCarritoEntity existingItem = existingItemOpt.get();
+            existingItem.setAmount(existingItem.getAmount() - 1);
+            return itemsCarritoRepository.save(existingItem);
+        }
+        return null;
+    }
+
+
 }
