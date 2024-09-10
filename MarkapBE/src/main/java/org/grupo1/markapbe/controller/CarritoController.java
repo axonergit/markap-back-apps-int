e116571205b5baca28b1648b70999fe363ce3cd5
mathiuSpv <mathius998@gmail.com>
2024-09-10 20:32:31 -0300

    feat(cart): added tool to CartController (testing pending)

--- MarkapBE/src/main/java/org/grupo1/markapbe/controller/CarritoController.java
+++ MarkapBE/src/main/java/org/grupo1/markapbe/controller/CarritoController.java
@@ -1,7 +1,70 @@
 package org.grupo1.markapbe.controller;
 
+import org.grupo1.markapbe.controller.dto.CarritoDTO;
+import org.grupo1.markapbe.controller.dto.ItemsCarritoDTO;
+import org.grupo1.markapbe.service.CarritoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/carrito")
 public class CarritoController {
-    // Acoplar a los Requerimientos
 
+    @Autowired
+    private CarritoService carritoService;
 
+    @GetMapping("/actual")
+    public ResponseEntity<?> getCarritoActivo() {
+        CarritoDTO carrito = carritoService.getActiveCarritoDTO()
+                .orElseGet(() -> carritoService.createCarritoDTO());
+        return ResponseEntity.ok(carrito);
+    }
+
+    @GetMapping("/actual/productos")
+    public ResponseEntity<?> getAllCarritoItemsActivo() {
+        CarritoDTO carrito = carritoService.getActiveCarritoDTO()
+                .orElseGet(() -> carritoService.createCarritoDTO());
+        Set<ItemsCarritoDTO> itemsCarrito = carritoService.getAllItemsByCarritoDTO(carrito)
+                .orElse(new HashSet<>());
+        return ResponseEntity.ok(itemsCarrito);
+    }
+
+    @GetMapping("/historial")
+    public ResponseEntity<?> getAllsCarritoHistorial() {
+        List<CarritoDTO> carrito = carritoService.getAllCarritos()
+                .orElse(new ArrayList<>());
+        return ResponseEntity.ok(carrito);
+    }
+
+    @GetMapping("/historial/items")
+    public ResponseEntity<?> getCarritoItems(@RequestBody CarritoDTO carritoDTO) {
+        Set<ItemsCarritoDTO> itemsCarrito = carritoService.getAllItemsByCarritoDTO(carritoDTO)
+                .orElse(new HashSet<>());
+        return ResponseEntity.ok(itemsCarrito);
+    }
+
+    @PutMapping("/paid")
+    public ResponseEntity<?> updateCarritoStatus() {
+        CarritoDTO carrito = carritoService.changeStatusCarritoToPaid();
+        carritoService.createCarritoDTO();
+        return ResponseEntity.ok(carrito);
+    }
+
+    @PutMapping("/add/{productId}")
+    public ResponseEntity<?> addItem(@PathVariable Long productId, @RequestParam(defaultValue = "1") int amount) {
+        ItemsCarritoDTO itemsCarrito = carritoService.addItemToCarrito(productId, amount);
+        return ResponseEntity.ok(itemsCarrito);
+    }
+
+    @PutMapping("/remove/{productId}")
+    public ResponseEntity<?> removeItem(@PathVariable Long productId, @RequestParam(defaultValue = "1") int amount) {
+        ItemsCarritoDTO itemsCarrito = carritoService.removeItemFromCarrito(productId, amount);
+        return ResponseEntity.ok(itemsCarrito);
+    }
 }
