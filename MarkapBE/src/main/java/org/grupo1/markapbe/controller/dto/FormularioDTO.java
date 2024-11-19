1d3cd454a1d8221b2120ecd55ad42385be60fa87
Matias Gonzalez <mgonzalez.1997@gmail.com>
2024-11-19 15:23:58 -0300

    Validaciones de maxima longitud de caracter

--- MarkapBE/src/main/java/org/grupo1/markapbe/controller/dto/FormularioDTO.java
+++ MarkapBE/src/main/java/org/grupo1/markapbe/controller/dto/FormularioDTO.java
@@ -13,12 +13,14 @@ public record FormularioDTO (
     String nombreCompleto,
 
     @NotEmpty(message = "La problematica es obligatoria")
+    @Size(max = 250, message = "Maximo 100 caracteres")
     String problematica,
 
     @Size(max = 5, message = "Maximo 5 fotos")
     List<String> fotoBase64,
 
     @NotEmpty(message = "La descripcion es obligatoria")
+    @Size(max = 5000, message = "Maximo 5000 caracteres")
     String descripcion
 
 ) {}
